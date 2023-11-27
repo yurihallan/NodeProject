a0fa17ffe9a64aa080bc45e9dfc689e1ad8c860b
Yuri Hallan <yurihallan9@gmail.com>
2023-11-27 16:04:52 -0400

    Create a structure for videos CRUD

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+import { fastify } from 'fastify';
+import { DatabaseMemory } from './database-memory.js'
+const server = fastify();
 
-import { createServer } from 'node:http';
-
-const server = createServer((request, response) => {
-  response.write("Hello world")
-    return response.end()
+//GET, POST, PUT, DELETE
+//POST localhost:3333/videos
+server.post('/videos', () =>{
+  return 'Hello World'
 })
 
 
-server.listen(3333);
+server.get('/videos', () =>{
+  return 'Hello World'
+})
+
+server.put('/videos/:id', () =>{
+  return 'Hello World'
+})
+
+server.delete('/videos/:id', () =>{
+  return 'Hello World'
+})
+
+server.listen({
+  port:3333,
+})
